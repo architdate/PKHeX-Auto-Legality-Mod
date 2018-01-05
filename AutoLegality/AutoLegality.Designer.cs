d9cdc69f874264ca7308f485180b3b71aa240aca
Archit Date <architdate@gmail.com>
2018-01-06 03:02:08 +0800

    Added key binding for Auto legality Mod
    Shortcut is Ctrl + I. closes #37

--- AutoLegality/AutoLegality.Designer.cs
+++ AutoLegality/AutoLegality.Designer.cs
@@ -7,6 +7,8 @@ namespace PKHeX.WinForms
             this.Menu_ShowdownImportPKMModded = new System.Windows.Forms.ToolStripMenuItem();
             this.Menu_ShowdownImportPKMModded.Image = ((System.Drawing.Image)(resources.GetObject("Menu_ShowdownImportPKM.Image")));
             this.Menu_ShowdownImportPKMModded.Name = "Menu_ShowdownImportPKMModded";
+            this.Menu_ShowdownImportPKMModded.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.I)));
+            this.Menu_ShowdownImportPKMModded.ShowShortcutKeys = false;
             this.Menu_ShowdownImportPKMModded.Size = new System.Drawing.Size(231, 22);
             this.Menu_ShowdownImportPKMModded.Text = "Import with Auto-Legality Mod";
             this.Menu_ShowdownImportPKMModded.Click += new System.EventHandler(this.ClickShowdownImportPKMModded);
