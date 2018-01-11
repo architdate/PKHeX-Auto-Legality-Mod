192ba9966e696cc0e703fef4b2f31db94d9786f9
Archit Date <architdate@gmail.com>
2018-01-12 02:40:13 +0800

    Export PGL -> Import PGL and add a Keybind
    Add an Alt + Q KeyBind

--- Addons (Optional)/PGL QR Auto Legality/PGLRentalLegality/PGLRentalLegality.Designer.cs
+++ Addons (Optional)/PGL QR Auto Legality/PGLRentalLegality/PGLRentalLegality.Designer.cs
@@ -7,8 +7,10 @@ namespace PKHeX.WinForms
             this.Menu_PGLShowdownSet = new System.Windows.Forms.ToolStripMenuItem();
             this.Menu_PGLShowdownSet.Image = ((System.Drawing.Image)(resources.GetObject("Menu_ShowdownExportPKM.Image")));
             this.Menu_PGLShowdownSet.Name = "Menu_PGLShowdownSet";
+            this.Menu_PGLShowdownSet.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Alt | System.Windows.Forms.Keys.Q)));
+            this.Menu_PGLShowdownSet.ShowShortcutKeys = false;
             this.Menu_PGLShowdownSet.Size = new System.Drawing.Size(231, 22);
-            this.Menu_PGLShowdownSet.Text = "Export PGL QR code";
+            this.Menu_PGLShowdownSet.Text = "Import PGL QR code";
             this.Menu_PGLShowdownSet.Click += new System.EventHandler(this.PGLShowdownSet);
             return this.Menu_PGLShowdownSet;
         }
