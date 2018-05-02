fdbad17052804d0faa462602f28660e0c2f49362
chenzw <chenzw25@gmail.com>
2018-05-02 12:14:40 +0800

    fix incomplete implementation of 00b934d

--- Addons (Optional)/MGDB Downloader/MGDBDownloader/MGDBDownloader.Designer.cs
+++ Addons (Optional)/MGDB Downloader/MGDBDownloader/MGDBDownloader.Designer.cs
@@ -3,14 +3,14 @@ namespace PKHeX.WinForms
 {
     partial class Main
     {
-        public System.Windows.Forms.ToolStripMenuItem EnableMGDBDownloader(System.ComponentModel.ComponentResourceManager resources, bool LatestCommit = false)
+        public System.Windows.Forms.ToolStripMenuItem EnableMGDBDownloader(System.ComponentModel.ComponentResourceManager resources)
         {
             this.Menu_MGDBDownloader = new System.Windows.Forms.ToolStripMenuItem();
             this.Menu_MGDBDownloader.Image = ((System.Drawing.Image)AutoLegality.AutoLegalityMod.mgdbdownload);
             this.Menu_MGDBDownloader.Name = "Menu_MGDBDownloader";
             this.Menu_MGDBDownloader.Size = new System.Drawing.Size(231, 22);
             this.Menu_MGDBDownloader.Text = "Download MGDB";
-            this.Menu_MGDBDownloader.Click += delegate(object sender, EventArgs e) { this.DownloadMGDB(sender, e, LatestCommit); };
+            this.Menu_MGDBDownloader.Click += delegate(object sender, EventArgs e) { this.DownloadMGDB(sender, e); };
             return this.Menu_MGDBDownloader;
         }
         private System.Windows.Forms.ToolStripMenuItem Menu_MGDBDownloader;
